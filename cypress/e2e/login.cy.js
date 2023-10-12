4c14a213024e9123ffe93800ca82b7cc2bfc4e9e
ranggaoct <76764720+ranggaoct@users.noreply.github.com>
2023-10-12 22:02:38 +0700

    first commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,19 @@
+describe('login',{testIsolation:false}, () => {
+  it('validasi sign in button', () => {
+    cy.visit('/')
+    cy.get('[type="button"]').contains('Sign In').click();
+    cy.url().should('contain','login')
+    cy.get('[type="text"]').should('be.visible')
+    cy.get('[type="password"]').should('be.visible')
+  })
+  it('login with valid email', () => {
+    cy.get('[type="text"]').should('be.visible').clear().type('ranggi@gmail.com');
+    cy.get('[type="password"]').should('be.visible').clear().type('12345678');
+    cy.get('[type="button"]').contains('Login').click();
+  })
+  it('Sign Out', () => {
+    cy.get('[class="dropdown-toggle btn btn-primary"]').click();
+    cy.get('[class="dropdown-item"]').contains('Sign Out').click();
+    cy.url().should('not.contain','login')
+  });
+})
